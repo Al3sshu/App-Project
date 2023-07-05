44ee0e20174faa57ebd7ab8a52c3194e12cdce40
alejandra <alejandra.humanes93@gmail.com>
2023-07-05 20:48:48 +0200

    sql mas tablas

--- Back-end/sql/creation.sql
+++ Back-end/sql/creation.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS style;
+DROP TABLE IF EXISTS face;
+DROP TABLE IF EXISTS colorimetry;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
@@ -19,6 +21,23 @@ CREATE TABLE IF NOT EXISTS style(
     id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     season TEXT NOT NULL UNIQUE,
     place TEXT NOT NULL,
+    hobbies TEXT NOT NULL, 
+    response TEXT NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS face(
+hair TEXT NOT NULL, 
+color TEXT NOT NULL, 
+eyes TEXT NOT NULL, 
+skin_tone TEXT NOT NULL, 
+response TEXT NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS colorimetry(
+    cold_colors TEXT NOT NULL,
+    warm_colors TEXT NOT NULL,
     response TEXT NOT NULL
 
 )
